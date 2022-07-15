452918320260dbc3df970b7c2a322adbb4c4aeb4
Yoshitomo Matsubara <yoshitom@uci.edu>
2022-07-14 21:17:13 -0700

    added an option to write out jsonl file

--- translation.py
+++ translation.py
@@ -1,13 +1,17 @@
 import argparse
+import json
+import os
+from pathlib import Path
+
 import boto3
 import yaml
-import os
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='AWS Translate with Boto3')
     parser.add_argument('--config', help='yaml file path')
     parser.add_argument('--src', nargs='+', help='src text file path(s)')
+    parser.add_argument('--dst', help='dst jsonl file path')
     return parser.parse_args()
 
 
@@ -39,19 +43,32 @@ def translate(src_text, client, translate_kwargs):
     return response
 
 
+def write_jsonl_file(src_file_paths, client, config, dst_file_path):
+    if dst_file_path is None:
+        print('`dst_file_path` cannot be None')
+        return
+
+    dst_file_path = os.path.expanduser(dst_file_path)
+    Path(dst_file_path).parent.mkdir(parents=True, exist_ok=True)
+    with open(dst_file_path, 'w') as fp:
+        for src_file_path in src_file_paths:
+            src_texts = load_src_texts(src_file_path)
+            for src_text in src_texts:
+                response = translate(src_text, client, config['translate'])
+                json.dump(response, fp)
+                fp.write('\n')
+
+
 def main(args):
     config = load_config(args.config)
     translate_config = config['translate']
     client = get_client(config.get('session'))
     src_file_paths = args.src
     if src_file_paths is None or len(src_file_paths) == 0:
-        translate(None, client, translate_config)
-
-    for src_file_path in args.src:
-        src_texts = load_src_texts(src_file_path)
-        for src_text in src_texts:
-            response = translate(src_text, client, config['translate'])
-            print(response)
+        response = translate(None, client, translate_config)
+        print(response)
+    else:
+        write_jsonl_file(src_file_paths, client, config, args.dst)
 
 
 if __name__ == '__main__':
